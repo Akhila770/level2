8100a23c1585095d5c8b702dde0f5a007bbc8739
Akhila770 <reddyakhila307@gmail.com>
2024-08-05 22:07:29 +0530

    Add files via upload

---
+++ level1_part1.cpp
@@ -0,0 +1,14 @@
+// level 2 question 1
+//write a loop program to print 1 to 5 on one by one
+#include<stdio.h>
+int main()
+{
+	int x;
+	x=1;
+	loop:if(x<=5)
+	{
+		printf("%d\n",x);
+		x++;
+		goto loop;
+	}
+}
